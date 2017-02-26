2cb34f976e517aef1a998732b9ab80d9823f882e
santoshiiita <iit2015139@iiita.ac.in>
2017-02-26 17:55:30 +0530

    Add files via upload

---
+++ MyLogger.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+public class MyLogger{
+static void logger(String[] s, int cnt){  
+	    try {  
+    	Logger logger = Logger.getLogger("My log"); 
+    	FileHandler fh = new FileHandler("log.txt",true);          
+        logger.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();  
+        fh.setFormatter(formatter); 
+        logger.setUseParentHandlers(true);
+        int k=0;
+        for(k=0;k<cnt;k++){
+        	logger.info(s[k]);
+        }
+        
+
+    } catch (SecurityException e) {  
+        e.printStackTrace();  
+    } catch (IOException e) {  
+        e.printStackTrace();  
+    }  
+
+    
+
+}
+}
